d009a8a124cff20c2ee82b95640e16d19658148e
Mila <48856476+kohenm63@users.noreply.github.com>
2021-06-16 14:23:12 +0300

    Add files via upload
    first commit

---
+++ script.js
@@ -0,0 +1,416 @@
+  /*  var bell = document.getElementById("myAudio"); 
+
+
+function playAudio() { 
+  bell.play(); 
+}  */
+
+
+
+
+function chapterOne() {
+  document.querySelector('.textFrame').src = "/1.html";
+  alertOne();
+ 
+
+
+
+} 
+
+function chapterTwo() {
+  document.querySelector('.textFrame').src = "/2.html";
+  alertTwo();
+  
+}
+
+function chapterThree() {
+  document.querySelector('.textFrame').src = "/3.html";
+  alertThree();
+  
+} 
+
+
+function chapterFour() {
+  document.querySelector('.textFrame').src = "/4.html";
+  alertFour();
+  
+} 
+
+function chapterFive() {
+  document.querySelector('.textFrame').src = "/5.html";
+  alertFive();
+}
+function chapterSix() {
+  document.querySelector('.textFrame').src = "/6.html";
+  alertSix();
+  
+} 
+function chapterSeven() {
+  document.querySelector('.textFrame').src = "/7.html";
+  alertSeven();
+  
+} 
+
+function chapterEight() {
+  document.querySelector('.textFrame').src = "/8.html";
+  alertEight();
+ 
+} 
+
+function chapterNine() {
+  document.querySelector('.textFrame').src = "/9.html";
+  alertNine();
+
+} 
+
+function chapterTen() {
+  document.querySelector('.textFrame').src = "/10.html";
+  alertTen();
+  
+} 
+
+function chapterEleven() {
+  document.querySelector('.textFrame').src = "/11.html";
+  alertEleven();
+  
+} 
+
+function chapterTwelve() {
+  document.querySelector('.textFrame').src = "/12.html";
+  alertTwelve();
+  
+}
+function chapterThirteen() {
+  document.querySelector('.textFrame').src = "/13.html";
+  alertThirteen();
+  
+}
+
+function chapterFourteen() {
+  document.querySelector('.textFrame').src = "/14.html";
+  alertFourteen();
+  
+}
+function chapterFifteen() {
+  document.querySelector('.textFrame').src = "/15.html";
+  alertFifteen();
+  
+}
+function chapterSixteen() {
+  document.querySelector('.textFrame').src = "/16.html";
+  alertSixteen();
+  
+}
+function chapterSeventeen() {
+  document.querySelector('.textFrame').src = "/17.html";
+  alertSeventeen();
+  
+}
+function chapterEighteen() {
+  document.querySelector('.textFrame').src = "/18.html";
+  alertEighteen();
+  
+}
+
+
+//custom alerts according to chapters ; chnge opacity to 1 when clickin x btn
+function bgFunction() {
+  document.getElementById("bg").style.opacity = "1";
+}
+function alertOne() {
+  const x = document.getElementById("alertOne");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+    
+   
+  } 
+}
+
+function alertTwo() {
+  var x = document.getElementById("alertTwo");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%"
+  } else {
+    x.style.display = "none";
+  } 
+  
+}
+
+function alertThree() {
+  const x = document.getElementById("alertThree");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+    
+   
+  } 
+}
+
+function alertFour() {
+  const x = document.getElementById("alertFour");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+    
+   
+  } 
+}
+
+function alertFive() {
+  const x = document.getElementById("alertFive");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+    
+   
+  } 
+}
+
+function alertSix() {
+  const x = document.getElementById("alertSix");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+    
+   
+  } 
+}
+
+function alertSeven() {
+  const x = document.getElementById("alertSeven");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+    
+   
+  } 
+}
+
+function alertEight() {
+  const x = document.getElementById("alertEight");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+    
+   
+  } 
+}
+
+function alertNine() {
+  const x = document.getElementById("alertNine");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+    
+   
+  } 
+}
+
+function alertTen() {
+  const x = document.getElementById("alertTen");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+    
+   
+  } 
+}
+
+function alertEleven() {
+  const x = document.getElementById("alertEleven");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+    
+   
+  } 
+}
+
+function alertTwelve() {
+  const x = document.getElementById("alertTwelve");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+    
+   
+  } 
+}
+
+function alertThirteen() {
+  const x = document.getElementById("alertThirteen");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+    
+   
+  } 
+}
+
+function alertFourteen() {
+  const x = document.getElementById("alertFourteen");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+    
+   
+  } 
+}
+
+function alertFifteen() {
+  const x = document.getElementById("alertFifteen");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+  } 
+}
+
+function alertSixteen() {
+  const x = document.getElementById("alertSixteen");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+  } 
+}
+
+function alertSeventeen() {
+  const x = document.getElementById("alertSeventeen");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+  } 
+}
+
+function alertEighteen() {
+  const x = document.getElementById("alertEighteen");
+  const bg = document.getElementById("bg")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    bg.style.opacity = "10%";
+  } else {
+    x.style.display = "none";
+  } 
+}
+
+/*notes */
+const addBtn = document.getElementById("add");
+
+const notes = JSON.parse(localStorage.getItem("notes"));
+
+if (notes) {
+    notes.forEach((note) => {
+        addNewNote(note);
+    });
+}
+
+addBtn.addEventListener("click", (e) => {
+    addNewNote();
+});
+
+function addNewNote(text = "") {
+    const note = document.createElement("div");
+    note.classList.add("note");
+
+    note.innerHTML = `
+        <div class="notes">
+            <div class="tools">
+                <button class="edit"><i class="fas fa-edit"></i></button>
+                <button class="delete"><i class="fas fa-trash-alt"></i></button>
+            </div>
+            <div class="main ${text ? "" : "hidden"}"></div>
+            <textarea class="${text ? "hidden" : ""}"></textarea>
+        </div>
+    `;
+
+    const editBtn = note.querySelector(".edit");
+    const deleteBtn = note.querySelector(".delete");
+
+    const main = note.querySelector(".main");
+    const textArea = note.querySelector("textarea");
+
+    textArea.value = text;
+    main.innerHTML = marked(text);
+
+    editBtn.addEventListener("click", (e) => {
+        main.classList.toggle("hidden");
+        textArea.classList.toggle("hidden");
+    });
+
+    deleteBtn.addEventListener("click", (e) => {
+        note.remove();
+
+        updateLS();
+    });
+
+    textArea.addEventListener("input", (e) => {
+        const { value } = e.target;
+
+        main.innerHTML = marked(value);
+
+        updateLS();
+    });
+
+    document.body.appendChild(note);
+}
+
+function updateLS() {
+    const notesText = document.querySelectorAll("textarea");
+
+    const notes = [];
+
+    notesText.forEach((note) => {
+        notes.push(note.value);
+    });
+
+    localStorage.setItem("notes", JSON.stringify(notes));
+}
